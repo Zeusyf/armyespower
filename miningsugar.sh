ecc230a6261526ae66fd7fca32f8714b92dd963e
Zeusyf <98632598+Zeusyf@users.noreply.github.com>
2022-03-14 04:09:41 +0700

    Update miningsugar.sh
    Newbug

--- miningsugar.sh
+++ miningsugar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while [ 1 ]; do
-./sugarmaker -a Yespowersugar -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qztmq7x3269atr20l0v577jlajna2qsuga9xv6t.er
+./sugarmaker -a YespowerSugar -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qztmq7x3269atr20l0v577jlajna2qsuga9xv6t.er
 sleep 5
 done
